73642ae0a936fa69485057d8aed6e191e6ba418f
Arsalan Gundroo <arsalang@tavisca.com>
2016-08-25 01:29:32 +0530

    Added token authentication middlewares

--- Advertisement-Service/app.js
+++ Advertisement-Service/app.js
@@ -4,7 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose  = require('mongoose');
+var mongoose = require('mongoose');
+var querystring = require('querystring');
+var http = require('http');
 var defaultRoutes = require('./routes/index');
 var users = require('./routes/users');
 var sellerRoutes = require('./routes/sellerRoutes.js');
@@ -22,19 +24,113 @@ mongoose.connect(connectionString);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
+sellerRoutes.use(function(req, res, next) {
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  if (token) {
+    var requestData = {
+      "token": token,
+      "user_role": "seller"
+    };
+    request({
+      url: "http://localhost:3300/access_authentication/",
+      method: "POST",
+      json: true,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(requestData)
+    }, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        console.log(body)
+        if (body.authenticity && body.authenticity === true) {
+          next();
+        } else {
+          res.status(403);
+          res.send({
+            message: "Invalid Access!!"
+          });
+        }
+      } else {
+        console.log("error: " + error);
+        console.log("response.statusCode: " + response.statusCode);
+        console.log("response.statusText: " + response.statusText);
+        return res.status(403).send({
+          success: false,
+          message: 'Could not authenticate the request.'
+        });
+      }
+    });
+  } else {
+    return res.status(403).send({
+      success: false,
+      message: 'No token provided.'
+    });
+  }
+});
+
+customerRoutes.use(function(req, res, next) {
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  if (token) {
+    var requestData = {
+      "token": token,
+      "user_role": "customer"
+    };
+    request({
+      url: "http://localhost:3300/access_authentication/",
+      method: "POST",
+      json: true,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(requestData)
+    }, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        console.log(body)
+        if (body.authenticity && body.authenticity === true) {
+          next();
+        } else {
+          res.status(403);
+          res.send({
+            message: "Invalid Access!!"
+          });
+        }
+      } else {
+        console.log("error: " + error);
+        console.log("response.statusCode: " + response.statusCode);
+        console.log("response.statusText: " + response.statusText);
+        return res.status(403).send({
+          success: false,
+          message: 'Could not authenticate the request.'
+        });
+      }
+    });
+  } else {
+    return res.status(403).send({
+      success: false,
+      message: 'No token provided.'
+    });
+  }
+});
+
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', defaultRoutes);
 app.use('/users', users);
-app.use('/seller',sellerRoutes);
-app.use('/customer',customerRoutes);
-app.use('/adImages',imageUploadRoute);
+app.use('/seller', sellerRoutes);
+app.use('/customer', customerRoutes);
+app.use('/adImages', imageUploadRoute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
